a5c9f5f80615cd56449a60fb23b63136bfa1bd2b
workgalta <workgalta@gmail.com>
2026-08-02 03:06:01 +0700

    style: expand brand color palette to ensure better contrast between brands

--- src/shared/utils/appUi.tsx
+++ src/shared/utils/appUi.tsx
@@ -247,30 +247,27 @@ export const getBrandColor = (brandName: string) => {
       border: "border-emerald-300",
     };
   const colors = [
-    {
-      bg: "bg-indigo-100",
-      text: "text-indigo-800",
-      border: "border-indigo-300",
-    },
-    {
-      bg: "bg-emerald-100",
-      text: "text-emerald-800",
-      border: "border-emerald-300",
-    },
+    { bg: "bg-red-100", text: "text-red-800", border: "border-red-300" },
+    { bg: "bg-orange-100", text: "text-orange-800", border: "border-orange-300" },
     { bg: "bg-amber-100", text: "text-amber-800", border: "border-amber-300" },
-    { bg: "bg-sky-100", text: "text-sky-800", border: "border-sky-300" },
-    {
-      bg: "bg-fuchsia-100",
-      text: "text-fuchsia-800",
-      border: "border-fuchsia-300",
-    },
+    { bg: "bg-yellow-100", text: "text-yellow-800", border: "border-yellow-300" },
+    { bg: "bg-lime-100", text: "text-lime-800", border: "border-lime-300" },
+    { bg: "bg-green-100", text: "text-green-800", border: "border-green-300" },
+    { bg: "bg-emerald-100", text: "text-emerald-800", border: "border-emerald-300" },
     { bg: "bg-teal-100", text: "text-teal-800", border: "border-teal-300" },
-    {
-      bg: "bg-orange-100",
-      text: "text-orange-800",
-      border: "border-orange-300",
-    },
+    { bg: "bg-cyan-100", text: "text-cyan-800", border: "border-cyan-300" },
+    { bg: "bg-sky-100", text: "text-sky-800", border: "border-sky-300" },
     { bg: "bg-blue-100", text: "text-blue-800", border: "border-blue-300" },
+    { bg: "bg-indigo-100", text: "text-indigo-800", border: "border-indigo-300" },
+    { bg: "bg-violet-100", text: "text-violet-800", border: "border-violet-300" },
+    { bg: "bg-purple-100", text: "text-purple-800", border: "border-purple-300" },
+    { bg: "bg-fuchsia-100", text: "text-fuchsia-800", border: "border-fuchsia-300" },
+    { bg: "bg-pink-100", text: "text-pink-800", border: "border-pink-300" },
+    { bg: "bg-rose-100", text: "text-rose-800", border: "border-rose-300" },
+    { bg: "bg-sky-200", text: "text-sky-900", border: "border-sky-400" },
+    { bg: "bg-emerald-200", text: "text-emerald-900", border: "border-emerald-400" },
+    { bg: "bg-fuchsia-200", text: "text-fuchsia-900", border: "border-fuchsia-400" },
+    { bg: "bg-orange-200", text: "text-orange-900", border: "border-orange-400" }
   ];
   let hash = 0;
   for (let i = 0; i < brandName.length; i++) {
