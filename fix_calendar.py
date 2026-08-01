1c34dbb622cd138f789f287021aa7a037cb85a83
workgalta <workgalta@gmail.com>
2026-08-02 02:42:58 +0700

    chore: cleanup python scripts

--- fix_calendar.py
+++
@@ -1,7 +0,0 @@
-with open('src/components/host/HostDashboard.tsx', 'r') as f:
-    content = f.read()
-
-content = content.replace('{renderCalendarDays(computedSchedules)}', '{renderCalendarDays()}')
-
-with open('src/components/host/HostDashboard.tsx', 'w') as f:
-    f.write(content)
